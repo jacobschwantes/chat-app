4e56d157d76e17d9e0f9440711edf62c0d78be19
skrt-dev <74641690+j-sch23@users.noreply.github.com>
2021-07-19 16:55:09 -0500

    Update config

--- chat-demo/src/services/firebase.js
+++ chat-demo/src/services/firebase.js
@@ -2,7 +2,9 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 const config = {
-
+    apiKey: "AIzaSyAEVrxtgQiz58Md__VDGWjlxiahNdjUD10",
+    authDomain: "chat-demo-c2549.firebaseapp.com",
+    databaseURL: "https://chat-demo-c2549-default-rtdb.firebaseio.com/"
 };
 
 firebase.initializeApp(config);
